5b966ac5ef5f99fcdfd90633d9b653be8c9c8b12
Gordon <gordonadamsnz@gmaill.com>
2022-04-17 15:01:30 +1200

    Updated main to use new config model

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from IT8951.display import AutoEPDDisplay
 from widgets.hello_world import *
 from widgets.quote_of_the_day import print_QOD
 from widgets.news import print_feeds
+from widgets.weather import print_weather
+from widgets.header import print_header
+from widgets.this_day_in_history import print_day
 
 minX=30
 minY=10
@@ -24,6 +27,12 @@ WHITE=255
 
 def main():
 
+    print('Initializing Config...')
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    print("Config Loaded:", config.sections())
+
     print('Initializing EPD...')
 
     # here, spi_hz controls the rate of data transfer to the device, so a higher
@@ -37,12 +46,23 @@ def main():
     canvas = display.frame_buf
     draw = ImageDraw.Draw(canvas)
 
+    print_header(draw, minX, minY, canvasWidth)
+
+    print_weather(canvas, draw, minX + canvasWidth//2 - 325, minY+70, config['weather']['lat'], config['weather']['lon'], config['weather']['apikey'])
+    lineOffset = 340
+    draw.line([minX + canvasWidth//2 - lineOffset, minY+100, minX + canvasWidth//2 - lineOffset, 650])
+    draw.line([minX + canvasWidth//2 + lineOffset, minY+100, minX + canvasWidth//2 + lineOffset, 650])
+
+    draw.line([minX+40, 680, maxX-40, 680])
+    print_day(draw, minX, 700, canvasWidth, config['dayInHistory']['token'], config['dayInHistory']['appName'])
+
+    draw.line([minX+40, 880, maxX-40, 880])
     print_QOD(draw, minX, 1000, canvasWidth)
 
-    nzFeeds=["https://www.rnz.co.nz/rss/national.xml", "https://www.stuff.co.nz/rss"]
-    print_feeds(draw, minX+10, 1100, canvasWidth//2, nzFeeds, "New Zealand")
-    worldFeeds=["https://www.rnz.co.nz/rss/world.xml", "http://feeds.bbci.co.uk/news/rss.xml", "https://rss.nytimes.com/services/xml/rss/nyt/World.xml", "https://moxie.foxnews.com/feedburner/latest.xml"]
-    print_feeds(draw, minX+canvasWidth//2, 1100, canvasWidth//2, worldFeeds, "World")
+    draw.line([minX+40, 1080, maxX-40, 1080])
+    print_feeds(draw, minX+10, 1100, canvasWidth//2, config['news']['localFeeds'].split(), config['news']['localFeedsName'])
+    draw.line([canvasWidth//2, 1150, canvasWidth//2, maxY-50])
+    print_feeds(draw, minX+10+canvasWidth//2, 1100, canvasWidth//2, config['news']['worldFeeds'].split(), "World")
 
     display.draw_full(constants.DisplayModes.GC16)
 
