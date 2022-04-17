646e68e7ad64e33666fffb852747f75245b2cec4
Gordon <gordonadamsnz@gmaill.com>
2022-04-18 09:21:19 +1200

    Refactored layout to be the same for both test and main.py

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-
 from time import sleep
+from pprint import pprint
 import argparse
-from PIL import Image, ImageDraw
 import configparser
+
+from layout import drawLayout
 
 from IT8951 import constants
 from IT8951.display import AutoEPDDisplay
-
-# widgets
-from widgets.hello_world import *
-from widgets.quote_of_the_day import print_QOD
-from widgets.news import print_feeds
-from widgets.weather import print_weather
-from widgets.header import print_header
-from widgets.this_day_in_history import print_day
-
-display = None
-
-minX=30
-minY=10
-maxX=1345
-maxY=1840
-boarderFrameWidth=10
-canvasWidth=maxX-minX
-canvasHeight = maxY - minY
-
-BLACK=0
-WHITE=255
 
 def main():
 
@@ -47,34 +27,21 @@ def main():
 
     print('Initializing buffer...')
     canvas = display.frame_buf
-    draw = ImageDraw.Draw(canvas)
+    
 
-    print_header(draw, minX, minY, canvasWidth)
+    drawLayout(canvas, config)
+    
 
-    print_weather(canvas, draw, minX + canvasWidth//2 - 325, minY+70, config['weather']['lat'], config['weather']['lon'], config['weather']['apikey'])
-    lineOffset = 340
-    draw.line([minX + canvasWidth//2 - lineOffset, minY+100, minX + canvasWidth//2 - lineOffset, 650])
-    draw.line([minX + canvasWidth//2 + lineOffset, minY+100, minX + canvasWidth//2 + lineOffset, 650])
-
-    draw.line([minX+40, 680, maxX-40, 680])
-    print_day(draw, minX, 700, canvasWidth, config['dayInHistory']['token'], config['dayInHistory']['appName'])
-
-    draw.line([minX+40, 880, maxX-40, 880])
-    print_QOD(draw, minX, 900, canvasWidth)
-
-    draw.line([minX+40, 1080, maxX-40, 1080])
-    print_feeds(draw, minX+10, 1100, canvasWidth//2, config['news']['localFeeds'].split(), config['news']['localFeedsName'])
-    draw.line([canvasWidth//2, 1150, canvasWidth//2, maxY-50])
-    print_feeds(draw, minX+10+canvasWidth//2, 1100, canvasWidth//2, config['news']['worldFeeds'].split(), "World")
-
-    display.draw_full(constants.DisplayModes.GC16)
+    # TODO: this will only work if we dont initialise it every time - fix it geirden
+    display.draw_partial(constants.DisplayModes.GC16)
 
     print('Done!')
 
     sleep(60)
     
+    display.clear()
 
 if __name__ == '__main__':
-    for i in range(5):
-        main()
-    display.clear()
+    # TODO: looks like we should run every 5 minutes for news updates, pytho cron???
+    # https://stackoverflow.com/a/16786600
+    main()
--- test.py
+++ test.py
@@ -1,30 +1,9 @@
-from pprint import pprint
 from time import sleep
-from PIL import Image, ImageDraw
+from pprint import pprint
+import argparse
 import configparser
 
-import argparse
-
-
-# widgets
-from widgets.hello_world import *
-from widgets.quote_of_the_day import print_QOD
-from widgets.news import print_feeds
-from widgets.weather import print_weather
-from widgets.header import print_header
-from widgets.this_day_in_history import print_day
-
-
-minX=30
-minY=10
-maxX=1345
-maxY=1840
-boarderFrameWidth=10
-canvasWidth=maxX-minX
-canvasHeight = maxY - minY
-
-BLACK=0
-WHITE=255
+from layout import drawLayout, testCanvas
 
 
 def main():
@@ -35,35 +14,10 @@ def main():
     pprint(config['weather'])
 
     print('Initializing buffer...')
-    canvas = Image.new('1', (canvasWidth, canvasHeight), WHITE)
-    draw = ImageDraw.Draw(canvas)
+    canvas = testCanvas()
+    
+    drawLayout(canvas, config)
 
-
-    # print_hello_world_centered(draw, canvasWidth//2, canvasHeight//2, "I keep asking myself these three questions.. What do you have? What do")
-
-    print_header(draw, minX, minY, canvasWidth)
-
-    print_weather(canvas, draw, minX + canvasWidth//2 - 325, minY+70, config['weather']['lat'], config['weather']['lon'], config['weather']['apikey'])
-    lineOffset = 340
-    draw.line([minX + canvasWidth//2 - lineOffset, minY+100, minX + canvasWidth//2 - lineOffset, 650])
-    draw.line([minX + canvasWidth//2 + lineOffset, minY+100, minX + canvasWidth//2 + lineOffset, 650])
-
-
-    # This gona be this day in history
-    draw.line([minX+40, 680, maxX-40, 680])
-    print_day(draw, minX, 700, canvasWidth, config['dayInHistory']['token'], config['dayInHistory']['appName'])
-
-    draw.line([minX+40, 880, maxX-40, 880])
-    print_QOD(draw, minX, 900, canvasWidth)
-
-    draw.line([minX+40, 1080, maxX-40, 1080])
-    print_feeds(draw, minX+10, 1100, canvasWidth//2, config['news']['localFeeds'].split(), config['news']['localFeedsName'])
-    draw.line([canvasWidth//2, 1150, canvasWidth//2, maxY-50])
-    print_feeds(draw, minX+10+canvasWidth//2, 1100, canvasWidth//2, config['news']['worldFeeds'].split(), "World")
-
-
-
-    draw.rectangle([minX,minY,maxX,maxY],outline=BLACK)
     print("Saving Image...")
     canvas.save("display.png")
     
