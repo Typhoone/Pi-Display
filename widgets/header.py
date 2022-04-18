80b5d420cbd06937a92e12719d7c114d33fa6925
Gordon <gordonadamsnz@gmaill.com>
2022-04-18 15:42:41 +1200

    Testing Wifi Signal Strenth

--- widgets/header.py
+++ widgets/header.py
@@ -1,2 +1,10 @@
+import subprocess
+
+def draw_wifi(draw, x, y, width):
+    wifiStrength = subprocess.run(['awk', 'NR==3 {print $3}', '/proc/net/wireless'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print("Wifi Stength: ", wifiStrength)
+
 def print_header(draw, x, y, width):
     draw.rectangle([x,y,x+width, y+50], fill=0)
+    draw_wifi(draw, x, y, width)
+    
